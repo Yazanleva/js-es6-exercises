e9dc69a5f55d02d82783dc4f7d751082f56058d9
Yazanleva <yazannalsalloumi@gmail.com>
2026-02-06 13:44:02 +0100

    TASK2: S2_FOR_01 - fizzbuzz with for loop

---
+++ js/task2/S2_FOR_01.js
@@ -0,0 +1,11 @@
+// S2_FOR_01 — FizzBuzz+
+
+for (let i = 1; i <= 30; i++) {
+  const fizz = i % 3 === 0;
+  const buzz = i % 5 === 0;
+
+  if (fizz && buzz) console.log("FizzBuzz");
+  else if (fizz) console.log("Fizz");
+  else if (buzz) console.log("Buzz");
+  else console.log(i);
+}
