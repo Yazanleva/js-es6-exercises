30babab7425998539598bd435c4896573f69b2ec
Yazanleva <yazannalsalloumi@gmail.com>
2026-02-06 13:47:20 +0100

    TASK3: S3_FN_03 - closure makeAdder returning arrow

---
+++ js/task3/S3_FN_03.js
@@ -0,0 +1,10 @@
+// S3_FN_03 — Closures
+
+const makeAdder = x => (y => x + y);
+
+// Tests
+const add5 = makeAdder(5);
+const add10 = makeAdder(10);
+
+console.log(add5(3));   // 8
+console.log(add10(3));  // 13
