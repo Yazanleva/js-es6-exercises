6c3b467ab06a09be73deb30b0cbeb8aa24dfd172
Yazanleva <yazannalsalloumi@gmail.com>
2026-02-06 13:34:23 +0100

    TASK2: S2_ARR_04 - flatten array one level

---
+++ js/task2/S2_ARR_04.js
@@ -0,0 +1,16 @@
+// S2_ARR_04 — Flatten 1 level
+
+function flatten1(arr) {
+  const result = [];
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      result.push(...item);
+    } else {
+      result.push(item);
+    }
+  }
+  return result;
+}
+
+// Tests
+console.log(flatten1([1, [2, 3], [4], 5])); // [1,2,3,4,5]
